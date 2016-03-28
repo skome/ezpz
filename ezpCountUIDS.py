433e9248b54ade714aa7b561d1a0f55f2b8cd9e7
Sam Kome <sam.kome@cuc.claremont.edu>
2016-03-28 10:33:49 -0700

    created campus counts and prettied them up

--- ezpCountUIDS.py
+++ ezpCountUIDS.py
@@ -16,18 +16,20 @@ if __name__ == '__main__':
 	ResFile = open(OutFileName,'w')
 	ezpwriter = csv.writer(ResFile, delimiter=',')
 	with open(InFileName,'r') as ezp:
-		uid = {}
+		uid = {} # unique user id dictionary: key is uid, value is campus
 		errors = defaultdict(int)
 		for line in ezp:
-			idatsrc = line[41:70].split('@')
+			idatsrc = line[41:70].split('@') # parse out the user id from the log line
 			try: 
-				uid[idatsrc[0].strip()] = idatsrc[1].split('.')[0].strip()#idatsrc[1].split('.')[0]
+				uid[idatsrc[0].strip()] = idatsrc[1].split('.')[0].strip()# note though that the uid prefix may not be unique
 			except IndexError:
 				errors[idatsrc[0]]+=1
 				#uid = idatsrc[0]
 				#campus = idatsrc[1]
-		campuses = uid.values()
-		campuses.sort()
-		cfreq = Counter(campuses)
-		print cfreq.items()
-		print errors
+		campuses = uid.values() 
+		campuses.sort()	
+		cfreq = Counter(campuses) # a list of tuples: campus, count
+		for k,v in cfreq.iteritems(): 
+			print '{0:15}{1:6d}'.format(k,v)
+		for k,v in errors.iteritems():
+			print 'Error Value {} occurred {} times'.format(k,v)
