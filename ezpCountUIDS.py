d2b34c4545652834875565e36e48d40caa113293
Sam Kome <sam.kome@cuc.claremont.edu>
2016-03-30 20:27:00 -0700

    cleanup

--- ezpCountUIDS.py
+++ ezpCountUIDS.py
@@ -16,20 +16,31 @@ if __name__ == '__main__':
 	ResFile = open(OutFileName,'w')
 	ezpwriter = csv.writer(ResFile, delimiter=',')
 	with open(InFileName,'r') as ezp:
-		uid = {} # unique user id dictionary: key is uid, value is campus
+		uidd = {} # unique user id dictionary: key is uid, value is campus
+		sidd = {} # session id dictionary: key is session, value is uid (hashed evenutually)
 		errors = defaultdict(int) # holds values that cause parsing errors as keys and count of them as value
 		for line in ezp:
 			idatsrc = line[41:70].split('@') # parse out the user id from the log line
-			try: 
-				uid[idatsrc[0].strip()] = idatsrc[1].split('.')[0].strip()# note though that the uid prefix may not be unique across campuses
+			uid = idatsrc[0].strip()
+			sessid = line[71:86]# count sessions per user
+			try: # assign campus (prefix) value to uid key
+				uidd[uid] = idatsrc[1].split('.')[0].strip()# note though that the uid prefix may not be unique across campuses
 			except IndexError:
 				errors[idatsrc[0]]+=1
-				#uid = idatsrc[0]
-				#campus = idatsrc[1]
-		campuses = uid.values() 
+			try: # assign uid value to session key
+				sidd[sessid] = uid
+			except:
+				  e = sys.exc_info()[0]
+				  print "Error: {}".format(e)
+		campuses = uidd.values() 
 		campuses.sort()	
 		cfreq = Counter(campuses) # a list of tuples: campus, count
 		for k,v in cfreq.iteritems(): 
 			print '{0:15}{1:6d}'.format(k,v)
+		users = sidd.values()
+		users.sort()
+		ufreq = Counter(users) # a list of tuples: user, count of sessions
+		for k,v in ufreq.iteritems():
+			print '{}, {}'.format(k,v)
 		for k,v in errors.iteritems():
 			print 'Error Value: "{}" occurred {} times'.format(k.strip(),v)
