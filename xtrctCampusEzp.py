7a733f9922264ee7ee3e7d0cc9dbe78e3581d615
Randall Nortman <rngithub@wonderclown.net>
2016-11-01 19:39:05 -0400

    Speed up xtrctCampusEzp by passing over log file only once
    This code is untested because I don't have valid sample data to test
    on.

--- xtrctCampusEzp.py
+++ xtrctCampusEzp.py
@@ -17,24 +17,27 @@ def printStatus(curr):
 	if curr%1000 == 0:
 		sys.stdout.write('Processing line {} / low millions\r'.format(curr))
 		sys.stdout.flush()
-		
+
 if __name__ == '__main__':
     ezpInFileName = sys.argv[1] #main log file
     ezpFilePrefix = ezpInFileName.split('.')[0]
     for campus in campuses:
         ezpOutFileName = ezpFilePrefix+"_"+campus+'.log'
         ezpReportFileName = cf_prefix+ezpFilePrefix+cf_post+campus+cf_type
-        print('Reading report: {}, searching {}').format(ezpReportFileName,ezpInFileName)
-        with open (ezpOutFileName,'w') as ezpOutFile, open(ezpInFileName, 'r') as ezpInFile, open(ezpReportFileName, 'r') as ezpCampusSessf:
-            sessions = []
-            for line in ezpCampusSessf.readlines():                                                                                                                
-                sessions.append(line[71:86])
-            print('Writing {}').format(ezpOutFileName)
-            for count, line in enumerate(ezpInFile):
-                printStatus(count)                                                                                                                            			
-                for session in sessions:
-                    #print session, line.split(' ')[2]
-                    if session in line.split(' ')[2]:
-                        ezpOutFile.writelines(line)
-                        break
+        outputFiles = {}
+        with open(ezpReportFileName, 'r') as ezpCampusSessf:
+            print('Reading report: {}, writing to {}').format(
+                ezpReportFileName,ezpOutFileName)
+            ezpOutFile = open (ezpOutFileName,'w')
+            for line in ezpCampusSessf.readlines():
+                session = line[71:86]
+                outputFiles[session] = ezpOutFile
+    with open(ezpInFileName, 'r') as ezpInFile:
+        print('Reading log {}').format(ezpInFileName)
+        for count, line in enumerate(ezpInFile):
+            printStatus(count)
+            session = line.split(' ')[2]
+            outfile = outputFiles.get(session)
+            if outfile is not None:
+                ezpOutFile.writelines(line)
     print("Completed.")
